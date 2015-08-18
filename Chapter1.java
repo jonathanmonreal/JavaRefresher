0d1c41b7ea6504a2b41d932a38f1a2e172c7ba37
jonathanmonreal <jem177@case.edu>
2015-08-18 13:20:19 -0400

    add problem11

--- Chapter1.java
+++ Chapter1.java
@@ -6,4 +6,13 @@ public class Chapter1 {
       System.out.printf("%d    %-2d     %-2d\n", a, (int)Math.pow(a, 2), (int)Math.pow(a, 3));
     }
   }
+  
+  public static void problem11() {
+    int population = 312032386;
+    for (int year = 0; year < 6; year++) {
+      if (year > 0)
+        System.out.printf("Year %d: %9d\n", year, population);
+      population += (60.0 / 7) * 24 * 365 + (60.0 / 45) * 24 * 365 - (60.0 / 13) * 24 * 365;
+    }
+  }
 }

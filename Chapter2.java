4bafec740fe5746c4699484b92b550c9e533fc16
jonathanmonreal <jem177@case.edu>
2015-08-20 13:38:13 -0400

    add problem2

--- Chapter2.java
+++ Chapter2.java
@@ -4,7 +4,18 @@ public class Chapter2 {
   public static void problem1() {
     Scanner in = new Scanner(System.in);
     
-    System.out.println("Enter a Celsius degree:");
+    System.out.print("Enter a Celsius degree:");
     System.out.println((9.0 / 5) * in.nextDouble() + 32);
+  }
+  
+  public static void problem2() {
+    Scanner in = new Scanner(System.in);
+    
+    System.out.print("Enter a radius and length of a cylinder:");
+    double radius = in.nextDouble();
+    double length = in.nextDouble();
+    double area = radius * radius * Math.PI;
+    System.out.println("The area is " + area);
+    System.out.println("The volume is " + area * length);
   }
 }
